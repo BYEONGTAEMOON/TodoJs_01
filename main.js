f4a0a722047f29d9a723ef54a1c42ac93eb49642
byeongtaemoon <byeongtaemoon@gmail.com>
2023-12-15 16:03:08 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+// 유저가 값을 입력한다
+// + 버튼을 클릭하면, 할일이 추가된다.
+// delete 버튼을 클릭하면 할일이 삭제된다
+// check 버튼을 클릭하면 할일이 끝나면서 밑줄이 간다.
+// 1. check 버튼을 클릭하는 순간 true false
+// 2. true이면 끝난걸로 밑줄
+
+// 진행 중 끝남 탭을 클릭하면, 언더바가 이동한다.
+// 끝남탭은, 끝난 아이템만, 진행 중 탭은 진행 중인 아이템만
+// 전체탭을 누르면 다시 전체 아이템으로 돌아옴
+
+let taskInput = document.getElementById('task-input');
+let addButton = document.getElementById('button-add');
+let taskList = [];
+
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+    let task = {
+        id: ramdomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    };
+    taskInput.value = '';
+    taskInput.focus();
+    taskList.push(task);
+    render();
+}
+
+function render() {
+    let resultHTML = '';
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].isComplete === true) {
+            resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button onClick ="toggleComplete('${taskList[i].id}')">check</button>
+                <button onClick ="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`;
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onClick ="toggleComplete('${taskList[i].id}')">check</button>
+                <button onClick ="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`;
+        }
+    }
+    document.getElementById('task-board').innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id === id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+}
+
+function deleteTask(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id === id) {
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
+}
+
+function ramdomIDGenerate() {
+    return '_' + Math.random().toString(36).substring(2, 9);
+}
